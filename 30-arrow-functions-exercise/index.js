ab63a84e8a5877e992f2886b45a71c599d4326e5
rrgthompson <rrgthomp@outlook.com>
2021-10-12 16:26:09 +0100

    Exercise 30

--- 30-arrow-functions-exercise/index.js
+++ 30-arrow-functions-exercise/index.js
@@ -9,12 +9,30 @@
  * and return true if they are equal or false if not
  */
 
+const isEqual = (num1, num2) => {
+    let result = num1 + num2;
+    if (result %2 == 0) 
+		return true;
+	else
+		return false;
+}
+
+console.log(isEqual(6,6));
+
 /**
  * Exercise 2
  *
  * create an arrow function {isLessOrEqual0} which takes number as a param
  * and return true if it equals or less than 0 or false if not
  */
+
+const isLessOrEqual0 = (evenNumber) => { 
+    if (evenNumber <= 0)
+    return true;
+    else 
+    return false;
+}
+
 
 /**
  * Exercise 3
@@ -27,6 +45,11 @@
  * so you will not break your function if you forget to pass params
  */
 
+var timeToSeconds = (hours = 0, minutes = 0) => {
+    return (hours * 60 + minutes) * 60;
+}
+console.log(timeToSeconds(10, 40));
+
 /**
  * Exercise 4
  *
@@ -36,6 +59,14 @@
  * pay then return true otherwise return false
  */
 
+var isGameWorthTheCandle = (probability,prize,pay) => {
+    if ((probability * prize) > pay)
+    return true;
+    else return false;
+}
+
+console.log()
+
 /**
  * Exercise 5
  *
@@ -43,3 +74,13 @@
  * as parameters and returns the string which is longest. If the strings are
  * equal in length return the string "Equal!"
  */
+
+
+var longerString = (stringOne,stringTwo) => {
+    if (stringOne.length < stringTwo.length) {
+        return stringTwo;
+      } else if (stringOne.length === stringTwo.length) {
+        return "Equal!";
+      }
+      return stringOne;
+    };
