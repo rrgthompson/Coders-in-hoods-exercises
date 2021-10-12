6d90bfceb27fec5d7fdd1b42d5de88f58b75975a
rrgthompson <rrgthomp@outlook.com>
2021-10-12 17:04:12 +0100

    exercise 31

--- 31-hof-forEach-exercise/index.js
+++ 31-hof-forEach-exercise/index.js
@@ -20,11 +20,23 @@
  * Exercise 1
  * create an arrow function {forEachCallback} that takes
  * param and logout it.
+ * 
+
  *
  * create a function {logAllWithExternalCallback} that will take an array
  * and with forEach log every element from your array
  * use {forEachCallback} as a callback function for forEach
  */
+
+var forEachCallback = (parameter) => {
+    console.log(parameter);
+}
+
+function logAllWithExternalCallback (array) {
+    array.forEach(forEachCallback);
+}
+
+console.log(logAllWithExternalCallback(["Apple","Pear","Orange","Cherry"]));
 
 /* =========================== */
 /* anonymous callbacks version */
@@ -35,6 +47,12 @@
  * do same thing as {logAllWithExternalCallback} but with
  * anonymous callback
  */
+
+ function logAllWithAnonymousCallback(array) {
+    array.forEach(element => {
+      console.log(element);
+    });
+  }
 
 /**
  * Exercise 2
@@ -49,6 +67,16 @@
  * Ex: dividableBy3([3,7,9,11]) => return [3,9]
  */
 
+var dividableBy3 = (array) => {
+    let filteredArray = [];
+  array.forEach(arrayElement => {
+    if (arrayElement % 3 === 0) {
+      filteredArray.push(arrayElement);
+    }
+  });
+  return filteredArray;
+};
+
 /**
  * Exercise 3
  * create an arrow function {oddNumbersSum} which takes an array
@@ -56,6 +84,17 @@
  *
  * Use forEach to loop through the array.
  */
+
+ var oddNumbersSum = (arrayOfNumbers) => {
+    let sum = 0;
+    arrayOfNumbers.forEach(element => {
+      if (element % 2 === 1) {
+        sum += element;
+      }
+    });
+    return sum;
+  };
+
 
 /**
  * Exercise 4
@@ -67,3 +106,14 @@
  *
  * Ex: ["cat", 5, "dog"] => 6
  */
+
+
+ var totalLength = (array) => {
+    let stringTotalLength = 0;
+    array.forEach(element => {
+      if (typeof element === "string") {
+        stringTotalLength += element.length;
+      }
+    });
+    return stringTotalLength;
+  };
