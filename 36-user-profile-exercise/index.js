dd8db161c7c44f42e06993b29060a080d038cff7
rrgthompson <rrgthomp@outlook.com>
2021-10-13 14:38:00 +0100

    Exercise 36

--- 36-user-profile-exercise/index.js
+++ 36-user-profile-exercise/index.js
@@ -1,4 +1,6 @@
 // install and  import "readline-sync" npm package before you do exercises
+var readlineSync = require('readline-sync');
+
 
 const user = {};
 /**
@@ -6,12 +8,17 @@ const user = {};
  *
  * ask user for a first name and save response to {user.firstName}
  */
+ user.firstName = readlineSync.question('May I have your first name? ');
+ console.log(`Hello ${user.firstName}!`);
+
 
 /**
  * Exercise 2
  *
  * ask user for a last name and save response to {user.lastName}
  */
+ user.lastName = readlineSync.question('May I have your last name? ');
+
 
 console.log(`Hello ${user.firstName} ${user.lastName}!`);
 
@@ -21,11 +28,17 @@ console.log(`Hello ${user.firstName} ${user.lastName}!`);
  * ask user for a age and save response to {user.age}
  */
 
+user.age = readlineSync.question('May I have your age? ');
+
+
 /*
  * Exercise 4
  *
  * ask user for a email and save response to {user.email}
  */
+
+user.email = readlineSync.question('May I have your email ');
+
 
 /**
  * OPTIONAL:
