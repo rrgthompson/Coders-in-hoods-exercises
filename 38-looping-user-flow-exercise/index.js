183e0b72f5300dd392131e97ddf77fcc53f0e2ae
rrgthompson <rrgthomp@outlook.com>
2021-10-14 16:25:36 +0100

    exercises 38

--- 38-looping-user-flow-exercise/index.js
+++ 38-looping-user-flow-exercise/index.js
@@ -1,39 +1,44 @@
 // install and  import "readline-sync" npm package before you do exercises
 
+const readlineSync = require("readline-sync");
+const chalk = require("chalk");
+const questionColor = message => chalk.cyanBright.bold(message);
+const answerColor = message => chalk.magentaBright.bold(message);
+
 let selectedItems = {
-	book: "",
-	movie: "",
-	nextTrip: ""
+  book: "",
+  movie: "",
+  nextTrip: "",
 };
 
 const movies = [
-	"The Invisible Man",
-	"Never Rarely Sometimes Always",
-	"Gretel & Hansel",
-	"No time to die",
-	"Bloodshot",
-	"Onward",
-	"Sonic"
+  "The Invisible Man",
+  "Never Rarely Sometimes Always",
+  "Gretel & Hansel",
+  "No time to die",
+  "Bloodshot",
+  "Onward",
+  "Sonic",
 ];
 
 const books = [
-	"My Dark Vanessa",
-	"Uncanny Valley",
-	"Weather",
-	"The night watchman",
-	"All adults here",
-	"Dear Edward",
-	"Grown ups"
+  "My Dark Vanessa",
+  "Uncanny Valley",
+  "Weather",
+  "The night watchman",
+  "All adults here",
+  "Dear Edward",
+  "Grown ups",
 ];
 
 const countries = [
-	"Italy",
-	"France",
-	"Germany",
-	"Spain",
-	"Portugal",
-	"Denmark",
-	"Netherland"
+  "Italy",
+  "France",
+  "Germany",
+  "Spain",
+  "Portugal",
+  "Denmark",
+  "Netherland",
 ];
 
 /**
@@ -46,6 +51,25 @@ const countries = [
  * NOTE: You need to add option to go back, to main menu
  */
 
+let selectedBookIndex;
+
+const chooseBook = () => {
+  selectedBookIndex = readlineSync.keyInSelect(
+    books,
+    "Which book would you like to read?"
+  );
+  selectedItems.book = books[selectedBookIndex];
+  if (selectedBookIndex === -1) {
+    console.log("Go back");
+    chooseBook();
+  }
+  console.log(`You will read ${selectedBookIndex}`);
+};
+
+chooseBook();
+console.log(`SELECTED BOOK IS`,selectedItems.book);
+
+
 /**
  * Exercise 2
  *
@@ -55,6 +79,24 @@ const countries = [
  *
  * NOTE: You need to add option to "go back", to main menu
  */
+
+ let selectedMovieIndex;
+
+ const chooseMovie = () => {
+   selectedMovieIndex = readlineSync.keyInSelect(
+	 movies,
+	 "Which movie would you like to watch?"
+   );
+   selectedItems.movie = movies[selectedMovieIndex];
+   if (selectedMovieIndex === -1) {
+	 console.log("Go back");
+	 chooseMovie();
+   }
+   console.log(`You will watch ${selectedMovieIndex}`);
+ };
+ 
+ chooseMovie();
+ console.log(`SELECTED Movie IS`,selectedItems.movie);
 
 /**
  * Exercise 3
@@ -66,6 +108,25 @@ const countries = [
  * NOTE: You need to add option to go back, to main menu
  */
 
+ let selectedCountryIndex;
+
+ const chooseCountry = () => {
+   selectedCountryIndex = readlineSync.keyInSelect(
+	 countries,
+	 "Which country would you like to visit?"
+   );
+   selectedItems.nextTrip = countries[selectedCountryIndex];
+   if (selectedCountryIndex === -1) {
+	 console.log("Go back");
+	 chooseCountry();
+   }
+   console.log(`You will visit ${selectedCountryIndex}`);
+ };
+ 
+ chooseCountry();
+ console.log(`SELECTED country IS`,selectedItems.nextTrip);
+
+
 /**
  * Exercise 4
  *
@@ -73,3 +134,27 @@ const countries = [
  * so user can pick one. User also should have the option "Exit".
  * When the user pick "Exit", log selected items.
  */
+
+
+ const menuSelected = readlineSync.keyInSelect(
+    ["Books", "Movies", "Next Destination"],
+    questionColor("Please choose a category: "),
+    {
+      cancel: "Exit"
+    }
+  );
+  switch (menuSelected) {
+    case 0:
+      chooseBook();
+      break;
+    case 1:
+      chooseMovie();
+      break;
+    case 2:
+      chooseCountry();
+      break;
+    default:
+    };
+  
+
+  menuSelected();
